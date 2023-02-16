af8b9553c215ff872128a54bf7ffd5fcdce2ac97
biruk <birukm8582@gmail.com>
2023-02-16 14:40:59 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * return: 0 (success)
  */
 
-int main()
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
