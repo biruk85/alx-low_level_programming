35aa57e3a709a51c2c828ecf4ebde2d5ec6332e9
biruk <birukm8582@gmail.com>
2023-02-16 14:36:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - script that prints aline of by puts function
  * return: 0 (success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print using the function printf
+ * return: 0 (success)
+ */
+
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
