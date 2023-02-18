22811fb4294af976c5e96ecceaeb87de84fcdbce
biruk <birukm8582@gmail.com>
2023-02-18 00:41:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - script that prints aline of by puts function
+ * main - script that prints a line of by puts function
  * return: 0 (success)
  */
 
@@ -10,3 +10,4 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
