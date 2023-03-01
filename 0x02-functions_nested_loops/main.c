74602215395684da24880bded0bc5b3238a57170
biruk <birukm8582@gmail.com>
2023-03-01 02:40:28 -0800

    main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,18 +1,34 @@
 #include "main.h"
 
+
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
-int main(void)
-{
-print_times_table(3);
-_putchar('\n');
-print_times_table(5);
-_putchar('\n');
-print_times_table(98);
-_putchar('\n');
-print_times_table(12);
+	int main(void)
+	{
+	    int r;
+
+
+	    r = print_sign(98);
+	    _putchar(',');
+	    _putchar(' ');
+	    _putchar(r + '0');
+	    _putchar('\n');
+	    r = print_sign(0);
+	    _putchar(',');
+	    _putchar(' ');
+	    _putchar(r + '0');
+	    _putchar('\n');
+	    r = print_sign(0xff);
+	    _putchar(',');
+	    _putchar(' ');
+	    _putchar(r + '0');
+	    _putchar('\n');
+	    r = print_sign(-1);
+	    _putchar(',');
+	    _putchar(' ');
+	    _putchar(r + '0');
+	    _putchar('\n');
 return (0);
-}
+	}
