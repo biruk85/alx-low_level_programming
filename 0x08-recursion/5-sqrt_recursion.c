ae41c7ffaca45cdb334ae2ac82ede6f1ddcd00fd
biruk <birukm8582@gmail.com>
2023-03-08 09:48:05 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ int _sqrt_recursion(int n)
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
  * @n: number to calculate the sqaure root of
- * @i: iterator
+ * @x: iterator
  * Return: the resulting square root
  */
 int actual_sqrt_recursion(int n, int x)
