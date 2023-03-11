ac7954fea33a7c7f7542131b0f2fe6b32516febd
biruk <birukm8582@gmail.com>
2023-03-11 02:11:56 -0800

    create_static_lib.sh

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - string
+ * Return: 0
+ */
+
+void _puts(char *str)
+
+#include<stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
